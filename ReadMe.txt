dbaa8e4665dea9d26c7f6325e7f69da904a69ef4
Abul Kashem <kashem.mcpd@gmail.com>
2017-07-29 17:09:59 +0600

    Mark completed task controller

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,4 @@
 rails g scaffold project title:string description:text
 rails generate model task content:string project:references
 rails generate controller tasks
+rails generate migration add_completed_at_to_task completed_at:datetime
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,14 @@
 class TasksController < ApplicationController
   before_action :set_project
+  before_action :set_task, except: [:create]
+
   def create
     @task = @project.tasks.create(task_params)
     redirect_to @project
   end
 
   def destroy
-    @task = @project.tasks.find(params[:id])
+    # @task = @project.tasks.find(params[:id])
     if @task.destroy
       flash[:success] = "Task deleted."
     else
@@ -15,13 +17,59 @@ class TasksController < ApplicationController
     redirect_to @project
   end
 
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @project, notice: "Task Completed"
+  end
+
   private
 
   def set_project
     @project = Project.find(params[:project_id])
   end
 
+  def set_task
+    # @task = @projcet.tasks.find(params[:id])
+    @task = @project.tasks.find(params[:id])
+  end
+
   def task_params
     params[:task].permit(:content)
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
